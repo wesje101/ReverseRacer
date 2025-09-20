223ad7cda675ef3ed2a941bfb6127444a0228c61
Wesley Verhagen <wesley.verhagen@gmail.com>
2025-09-20 14:20:06 +0200

    what the car

--- Scripts/CarController.cs
+++ Scripts/CarController.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class CarController : VehicleBody3D
 {
+    [Export] float maxSteer = 0.9f;
+    [Export] float steerSpeed = 10f;
+    [Export] float enginePower = 300;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Steering = (float)Mathf.MoveToward(Steering, Input.GetAxis("right", "left") * maxSteer, delta * steerSpeed);
+        EngineForce = Input.GetAxis("down", "up") * enginePower;
+    }
 }
